4e0e02b2da60f555f916987819f4db66f1a33f5f
Geza Jenes <jenesg@gmail.com>
2021-01-28 13:02:32 +0100

    Initial Commit

---
+++ DevoraDemo/DevoraDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  DevoraDemoApp.swift
+//  DevoraDemo
+//
+//  Created by Géza MacBook Pro on 2021. 01. 28..
+//
+
+import SwiftUI
+
+@main
+struct DevoraDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
